a7cae9d2ef8e2a3141b22735fec00bf61e5f63a7
KZoubek <91133180+KZoubek@users.noreply.github.com>
2022-09-05 07:53:22 +0200

    added servers and installed node

---
+++ server.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+
+const fs = require('fs');
+
+const router = require('./api/routes/router.js');
+
+const app = express();
+const port = 3456;
+
+app.use(session({
+    secret: 'cats',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 3600,
+        httpOnly: false
+    }
+}))
+
+app.use(express.static(path.join(__dirname, 'files')));
+
+// Parse urlencoded bodies (for form data)
+app.use(bodyParser.urlencoded({ extended: true }));
+// Parse JSON bodies (from requests)
+app.use(bodyParser.json());
+
+
+app.use('/api', router);
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`Server listening at http://localhost:${port}`)
+    }
+});
+
+//for session management
+app.use(session({
+    secret: 'topsecret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+
